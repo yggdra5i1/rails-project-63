40420a5a2eeebf55f4a03aa7be6b99307a6fe6e4
Maksim Shirokov <m5d3v@mail.ru>
2023-06-01 23:46:51 +0500

    Adds form_for specs

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,4 +5,10 @@ require_relative "hexlet_code/version"
 # Main namespace
 module HexletCode
   autoload :Tag, "hexlet_code/tag"
+
+  extend self
+
+  def form_for(object, &block)
+    return nil
+  end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -2,12 +2,18 @@
 
 require "test_helper"
 
+User = Struct.new(:name, :job, keyword_init: true)
+
 class TestHexletCode < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_it_does_something_useful
-    assert true
+  def test_form_for
+    user = User.new name: 'rob'
+    res = HexletCode.form_for user do |f|
+    end
+    expected_res = '<form action="#" method="post"></form>'
+    assert_equal res, expected_res
   end
 end
