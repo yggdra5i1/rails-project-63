a7a57fae2232ed89beff99a0c5b9de71537a1bec
Maksim Shirokov <m5d3v@mail.ru>
2023-06-03 23:52:21 +0500

    Adds prototype of input

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,10 +6,42 @@ require_relative "hexlet_code/version"
 module HexletCode
   autoload :Tag, "hexlet_code/tag"
 
+  INPUT_ALIASES = {
+    text: "textarea"
+  }.freeze
+
+  DEFAULT_INPUT_OPTS = {
+    text: { cols: "20", rows: "40" },
+    input: { type: "text" }
+  }.freeze
+
   module_function
 
-  def form_for(_object, options = {})
+  def form_for(object, options = {}, &block)
+    set_object(object)
     action = options[:url] || "#"
-    Tag.build("form", action: action, method: "post")
+    method = options[:method] || "post"
+    block.call(self) if block_given?
+
+    Tag.build("form", action: action, method: method)
+  end
+
+  def input(name, options = {})
+    value = object.public_send(name)
+    return if value.nil?
+
+    as = options.delete(:as)
+    tag_name = INPUT_ALIASES[as] || "input"
+    opts = options.any? ? options : DEFAULT_INPUT_OPTS[tag_name.to_sym]
+
+    Tag.build(tag_name, *opts) { value }
+  end
+
+  def set_object(obj)
+    @object = obj
+  end
+
+  def object
+    @object
   end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -2,13 +2,9 @@
 
 require "test_helper"
 
-User = Struct.new(:name, :job, keyword_init: true)
+User = Struct.new(:name, :job, :gender, keyword_init: true)
 
 class TestHexletCode < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::HexletCode::VERSION
-  end
-
   def test_form_for
     user = User.new name: "rob"
     res = HexletCode.form_for user do |f|
@@ -23,5 +19,14 @@ class TestHexletCode < Minitest::Test
     end
     expected_res = '<form action="/users" method="post"></form>'
     assert_equal res, expected_res
+  end
+
+  def test_form_for_with_inputs
+    user = User.new name: "rob", job: "hexlet", gender: "m"
+
+    HexletCode.form_for user do |f|
+      f.input :name
+      f.input :job, as: :text
+    end
   end
 end
